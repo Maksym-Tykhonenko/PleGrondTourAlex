55351f6952ddd07f21af7d8f8ae4b5a4e21a3dc6
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-12-18 12:28:19 +0200

    fixed date

--- PleGrondTourSrc/PleGrondTourRoutes/PleGrondTourStack.js
+++ PleGrondTourSrc/PleGrondTourRoutes/PleGrondTourStack.js
@@ -337,7 +337,7 @@ const PleGrondTourStack = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-12-07T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-12-20T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
